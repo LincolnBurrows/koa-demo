88843567cc79df44c27dfeaf6f076df610f52836
Sp0rt6ack <lincolnburrows1990@gmail.com>
2022-03-16 17:24:19 +0800

    add mysql module

--- router/manage/index.js
+++ router/manage/index.js
@@ -1,8 +1,19 @@
 const Router = require('koa-router')
 const router = new Router()
 
+const utils = require('../../utils')
+
+// 假设用户访问/manage即可查询数据库cms中user表的数据
 router.get('/', async ctx=>{
-    ctx.body = "管理系统"
+    let data = await new Promise((resolve, reject)=>{
+        //sql查询语句
+        var sql = 'select * from user'
+        utils.query(sql, (err, data)=>{
+            if(err) reject(err)
+            resolve(data)
+        }) 
+    })
+    ctx.body = data
 })
 
 
--- utils.js
+++ utils.js
@@ -5,7 +5,27 @@ const host = "http://localhost";
 
 const port = 9000;
 
+// 创建mysql连接池
+const mysql = require('mysql')
+const pool = mysql.createPool({
+    host:"localhost",
+    port: 3306,
+    database:'cms',
+    user:'root',
+    password:'123456'
+})
+// 数据库进行增删改查操作的基础
+const query = (sql,callback)=>{
+    pool.getConnction(function(err, connection){
+        connection.query(sql, function(err, rows){
+            callback(err, rows)
+            connection.release()
+        })
+    })
+}
+
 module.exports = {
     host,
-    port
+    port,
+    query
 }
